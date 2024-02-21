52abc483b3440c0757a5d8b65aa329405d0560da
Fenice <{ID}+{username}@users.noreply.github.com>
2024-02-21 11:35:20 +0100

    changes to the title

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Community answered questions about Asoiaf">
       <HomepageHeader />
       <main>
