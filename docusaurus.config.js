d51b045060e84f8b620705854e19d7af7f0a599b
Fenice <{ID}+{username}@users.noreply.github.com>
2024-02-22 13:36:09 +0100

    refresh

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Read the Fabulous Asoiaf Manual',
-  tagline: 'Unofficial community answers to common rules questions',
+  tagline: 'Unofficial community answers to common rules questions about ASOIAF TMG',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
